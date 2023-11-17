9a7cff6af2fe146e7e1b96c7e5c3e7b35195e1aa
Alien Brain <murad.dub@gmail.com>
2023-11-18 04:01:57 +0600

    user stored in db successfully

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.port || 5000;
 require("dotenv").config();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
-// Mongodb
+// Mongodb code
 console.log(process.env.DB_USER, process.env.DB_SECRET);
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,16 +25,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    // JWT related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    const userCollection = client.db("FSCafeDB").collection("users");
+
+    // User related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const alreadyExistUser = await userCollection.findOne(query);
+      if (alreadyExistUser) {
+        return res.send({ message: "User already Exists!", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
