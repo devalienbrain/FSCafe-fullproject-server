f9bff16ee48037fc71b93829f3f5198a674daceb
Alien Brain <murad.dub@gmail.com>
2023-11-20 03:03:31 +0600

    jwt and verifytoken doing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 // Mongodb code
 console.log(process.env.DB_USER, process.env.DB_SECRET);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.m38robg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -27,23 +27,41 @@ async function run() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Pinged.. Successfully connected to MongoDB!");
 
     // JWT related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
+      // console.log(req.headers);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res.send({ token });
     });
 
+    // middleware to verify token
+    const verifyToken = (req, res, next) => {
+      // console.log("Inside verifyToken:", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "forbidden access" });
+      } else {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log("Token:", token);
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: "forbidden access" });
+          }
+          req.decoded = decoded;
+          next();
+        });
+      }
+    };
+
     const userCollection = client.db("FSCafeDB").collection("users");
 
     // User related api
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
+      console.log(req.headers.authorization);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
@@ -56,6 +74,34 @@ async function run() {
         return res.send({ message: "User already Exists!", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    // handle make a user admin api
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedRole = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedRole);
+      res.send(result);
+    });
+
+    // Cart items related api
+    const cartCollection = client.db("FSCafeDB").collection("cart");
+
+    app.get("/cart", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
   } finally {
