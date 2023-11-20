c140677c97a5ba259eef6bde8c7de2aaa8a98e35
Alien Brain <murad.dub@gmail.com>
2023-11-20 22:50:15 +0600

    verify admin done

--- index.js
+++ index.js
@@ -57,13 +57,40 @@ async function run() {
       }
     };
 
+    // middleware to verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
     const userCollection = client.db("FSCafeDB").collection("users");
 
     // User related api
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       console.log(req.headers.authorization);
       const result = await userCollection.find().toArray();
       res.send(result);
+    });
+
+    // Call api from useAdmin client side
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     app.post("/users", async (req, res) => {
